b1e38730dda1d38407f6c1817d157892ea2f86cc
fanyongchao <fan2522@qq.com>
2025-01-22 14:28:47 +0800

    https

--- web/app/lib/api.ts
+++ web/app/lib/api.ts
@@ -1,6 +1,6 @@
 import "whatwg-fetch";
 
-const SERVER_PROTOCOL = process.env.NEXT_PUBLIC_ADH_SERVER_PROTOCOL || "http";
+const SERVER_PROTOCOL = process.env.NEXT_PUBLIC_ADH_SERVER_PROTOCOL || "https";
 const SERVER_PORT = process.env.NEXT_PUBLIC_ADH_SERVER_PORT || "8000";
 const VERSION = process.env.NEXT_PUBLIC_ADH_SERVER_VERSION || "v0";
 
